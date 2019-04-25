abae607a535d005078c0d6c1265b694bd078a320
susu105 <susu_105@hotmail.com>
2019-04-25 12:26:13 +0200

    Arreglat importació al erp
    Afegit un semàfor per intentar millorar rendiment de la tasca

--- massive_importer/lib/erp_utils.py
+++ massive_importer/lib/erp_utils.py
@@ -16,22 +16,23 @@ class ErpManager(object):
             verbose=verbose
         )
     
-    def get_file_b64content(content):
+    def get_file_b64content(self, content):
         return base64.encodebytes(content).decode()
 
-    def import_wizard(self, file_name, file_content):
-        values = {'file': file_content, 'filename' : file_name}
+    def import_wizard(self, file_name, file_content, mutex):
+        values = {'filename': file_name, 'file': self.get_file_b64content(file_content)}
+        mutex.acquire()
         WizardImportAtrF1 = self.erp_client.model('wizard.import.atr.and.f1')
         import_wizard = WizardImportAtrF1.create(values)
-        
         context = {'active_ids': [import_wizard.id], 'active_id': import_wizard.id}         
-        try:
-            res = import_wizard.action_import_xmls()
+        try:  
+            res = import_wizard.action_import_xmls(context)
         except Exception as e:
             msg = "An error ocurred importing %s: %s"
             logger.exception(msg, file_name, str(e))
             result = False
         else:
             result = True
-        
+            
+        mutex.release()
         return result
