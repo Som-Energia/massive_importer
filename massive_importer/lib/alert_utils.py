39e2575984832df54f063ed9fa8b68c6d30402b6
Xavier Terés <xavier.teres@somenergia.coop>
2022-02-02 12:16:09 +0100

    improve daily report template

--- massive_importer/lib/alert_utils.py
+++ massive_importer/lib/alert_utils.py
@@ -4,6 +4,7 @@ import smtplib, urllib, datetime, ssl
 from email import message
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
 from massive_importer.lib.exceptions import TooFewArgumentsException
 from mako.template import Template
 
@@ -42,7 +43,7 @@ class AlertManager(object):
         except Exception as e:
             logger.error("Error sending alert: %s", e)
 
-    def new_summary_send(self, date_events_task, i_date, f_date, event_list, importfile_list, errors_list):
+    def summary_send(self, date_events_task, i_date, f_date, event_list, importfile_list, errors_list):
         if i_date is None or f_date is None:
             raise TooFewArgumentsException('Too few arguments on summary send: dates missing')
         else:
@@ -57,80 +58,45 @@ class AlertManager(object):
             for event in event_list:
                 fail.append({
                     'name' : event.value['Records'][0]['s3']['object']['userMetadata']['X-Amz-Meta-Portal'],
-                    'success' : False,
-                    'description': 'No s\'ha importat a l\'ERP'
+                    'description': 'S\'ha descarregat però no s\'ha importat a l\'ERP'
                 })
             for impf in importfile_list:
                 correcte = impf.state == 'ok'
                 import_file = {
-                        'name' : urllib.parse.unquote(impf.name),
-                        'success' : correcte,
-                        'description': 'Importat correctament' if correcte else 'Hi ha hagut un error a l\'importar'
+                        'name' : impf.portal,
+                        'description': 'Importat correctament' if correcte else 'S\'ha descarregat però hi ha hagut un error a l\'importar'
                 }
                 if correcte:
-                    correcte.append(import_file)
+                    success.append(import_file)
                 else:
                     fail.append(import_file)
 
             for error in errors_list:
                 fail.append({
                     'name' : error.crawler_name,
-                    'success' : False,
-                    'description': error.exception_type + ': ' + error.description
+                    'description': error.description
                 })
 
-            plantilla = Template(filename='../../templates/daily_report.mako')
+            plantilla = Template(filename='templates/daily_report.mako')
             render = plantilla.render(success = success, fail = fail, interval = interval)
+
+            fp = open('templates/success.png', 'rb')
+            success_img = MIMEImage(fp.read())
+            fp.close()
+            success_img.add_header('Content-ID', '<image1>')
+
+            fp = open('templates/fail.png', 'rb')
+            fail_img = MIMEImage(fp.read())
+            fp.close()
+            fail_img.add_header('Content-ID', '<image2>')
 
             part1 = MIMEText(render, "html")
             message = MIMEMultipart("alternative")
             message.add_header('subject', "Resum importacio del dia " + i_date.strftime("%d-%m-%Y"))
             message.attach(part1)
+            message.attach(fail_img)
+            message.attach(success_img)
 
-            context = ssl.create_default_context()
-            with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-                server.login(self.from_addr, self.passwd)
-                server.sendmail(self.from_addr, self.to_addr, message.as_string())
-                return True
-
-    def summary_send(self, date_events_task, i_date, f_date, event_list, importfile_list, errors_list):
-        def green_red_ok(state):
-            if state == True: state = 'ok'
-            if state == False: state = 'error'
-            if(state=='ok'):
-                return "<font color='#00ff00'>"+ state +"</font>"
-            else:
-                return "<font color='#FF0000'>"+ state +"</font>"
-        def last_date(date):
-            if date is None:
-                return 'Data no disponible'
-            else:
-                return date.strftime("%Y-%m-%d %H:%M:%S")
-
-        if i_date is None or f_date is None:
-            raise TooFewArgumentsException('Too few arguments on summary send: dates missing')
-        else:
-            events = []; importfiles = []; errors = []
-            for event in event_list : events.append(event.key)
-            for impf in importfile_list : importfiles.append(urllib.parse.unquote(impf.name) + " < " + green_red_ok(impf.state) +" >")
-            for error in errors_list : errors.append( "< " + green_red_ok('error') + " > " + error.crawler_name + ": " + error.exception_type + " --- " + error.description)
-
-            _events = ""; _impfs = ""; _errors = ""
-            if(events):
-                _events = "<br><b>WARNING: Hi ha fitxers que no s'han importat al erp:</b><br>" + ("<br>".join(events)) +"<br>"
-            if(importfiles):
-                _impfs =  "Casos importats amb Estat: <br>" + ("<br>".join(importfiles)) +"<br>"
-            if(errors):
-                _errors =  "Errors durant la descàrrega de fitxers: <br>" + ("<br>".join(errors)) +"<br>"
-
-            main_task = ("<b>RESUM DE DESCÀRREGA I IMPORTACIÓ AMB L'INTERVAL: " + i_date.strftime("%Y-%m-%d %H:%M:%S") + " fins el "+ f_date.strftime("%Y-%m-%d %H:%M:%S")) + "</b>"
-            html = "<html><head></head><body>"+ main_task + "<br><br>" + _impfs + "<br>" + _errors + "<br>" + _events + "</body></html>"
-
-            part1 = MIMEText(html, "html")
-            message = MIMEMultipart("alternative")
-            subject = "Resum importacio del dia " + i_date.strftime("%Y-%m-%d")
-            message.add_header('subject', subject)
-            message.attach(part1)
             context = ssl.create_default_context()
             with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
                 server.login(self.from_addr, self.passwd)
--- templates/daily_report.mako
+++ templates/daily_report.mako
@@ -1,89 +1,36 @@
 <!DOCTYPE html>
 <html>
 <head>
-<style>
 
-body {
-  font-family: sans-serif;
-}
-
-table {
-  border-collapse: collapse;
-  width: 100%;
-}
-
-td, th {
-  border: 2px solid white;
-  text-align: left;
-  padding: 12px;
-}
-
-tr:nth-child(odd) {
-  background-color: #efefef;
-}
-
-tr:first-child {
-  background-color: #bac92c;
-  color: white;
-}
-
-#intervals {
-  font-size: 18px;
-}
-
-tr:last-child td {
-  border: 0px solid #bac92c;
-}
-
-table {
-  border-bottom: 4px solid #bac92c;
-}
-
-td:nth-child(2) {
-  text-align: center;
-}
-
-img {
-  width: 20px;
-}
-
-div {
-  padding: 12px;
-  border-bottom: 2px solid #bac92c;
-}
-
-</style>
 </head>
-<body>
+<body style="font-family: sans-serif;">
 
 <h2>INFORME IMPORTACIÓ MASSIVA</h2>
-<p id="intervals">Amb interval de <strong>${interval['inici']}</strong> a <strong>${interval['final']}</strong></p>
-<table>
-  <tr>
-    <th>Portal</th>
-    <th>Ha anat bé?</th>
-    <th>Observacions</th>
+<p id="intervals" style="font-size: 18px;">Amb interval de <strong>${interval['inici']}</strong> a <strong>${interval['final']}</strong></p>
+<table style="border-collapse: collapse;width: 100%;border-bottom: 4px solid #bac92c;">
+  <tr style="background-color: #bac92c;color: white;">
+    <th style="border: 2px solid white;text-align: left;padding: 12px;">Portal</th>
+    <th style="border: 2px solid white;text-align: left;padding: 12px;">Ha anat bé?</th>
+    <th style="border: 2px solid white;text-align: left;padding: 12px;">Observacions</th>
   </tr>
 % for crawler in success:
   <tr>
-    <td>${crawler['name']}</td>
-    <td><img src="./success.png" alt="Sí"/></td>
-    <td>${crawler['description']}</td>
+    <td style="border-bottom: 1px solid #bac92c;text-align: left;padding: 12px;">${crawler['name']}</td>
+    <td style="border-bottom: 1px solid #bac92c;text-align: center;padding: 12px;"><img src="cid:image1" alt="Sí" style="width: 20px;"></td>
+    <td style="border-bottom: 1px solid #bac92c;text-align: left;padding: 12px;">${crawler['description']}</td>
   </tr>
 % endfor
 % for crawler in fail:
   <tr>
-    <td>${crawler['name']}</td>
-    <td><img src="./fail.png" alt="No"/></td>
-    <td>${crawler['description']}</td>
+    <td style="border-bottom: 1px solid #bac92c;text-align: left;padding: 12px;">${crawler['name']}</td>
+    <td style="border-bottom: 1px solid #bac92c;text-align: center;padding: 12px;"><img src="cid:image2" alt="No" style="width: 20px;"></td>
+    <td style="border-bottom: 1px solid #bac92c;text-align: left;padding: 12px;">${crawler['description']}</td>
   </tr>
 % endfor
 </table>
-<div>
+<div id="resum" style="padding: 12px;border-bottom: 2px solid #bac92c;">
   <p><strong>Importats correctament:</strong> ${len(success)}</p>
   <p><strong>No s'han pogut importar:</strong> ${len(fail)}</p>
 </div>
 </body>
 </html>
-
-
